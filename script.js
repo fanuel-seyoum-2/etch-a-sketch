a3f58ff41a9bf9cb1530dbbd366a01516de02e2b
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-17 19:30:09 +0300

    Add function to create NbyN grid

---
+++ script.js
@@ -0,0 +1,15 @@
+function createNbyNGrid(N, container) {
+	let parDiv = document.querySelector(container);
+	for (let j = 0; j < N; j++) {	
+		let parentDiv = document.createElement("div");
+		parentDiv.setAttribute("class", `row row${j}`);
+
+		for (let i = 0; i < N; i++) {
+			let div = document.createElement("div");
+			div.setAttribute("class", `col col${i}`);
+			parentDiv.appendChild(div);
+		}
+		parDiv.appendChild(parentDiv);
+	}
+}
+createNbyNGrid(4, ".container");
