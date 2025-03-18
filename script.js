e369fb6e63b9a40bbeffbd95bddb7ccd7694620c
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-18 14:57:07 +0300

    Modify createNbyNGrid function, now createNbyMGrid - The number of rows and cols that can be created no longer need to be the same - Added comments

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-function createNbyNGrid(N, container) {
+/* createNbyMGrid
+ *	Creates an N by M grid of squares and appends them to a container
+ */
+function createNbyMGrid(N, M, container) {
 	let parDiv = document.querySelector(container);
 	for (let j = 0; j < N; j++) {	
 		let parentDiv = document.createElement("div");
 		parentDiv.setAttribute("class", `row row${j}`);
-
-		for (let i = 0; i < N; i++) {
+		// New "col" divs will be created and added to a "row" div
+		for (let i = 0; i < M; i++) {
 			let div = document.createElement("div");
 			div.setAttribute("class", `col col${i}`);
 			parentDiv.appendChild(div);
 		}
+		// The "row" div, containing M "col" div 
+		// is appended to the parDiv or "container"
 		parDiv.appendChild(parentDiv);
 	}
 }
-createNbyNGrid(16, ".container");
+createNbyMGrid(16, 16, ".container");
 
 let container = document.querySelector(".container");
 container.addEventListener("mouseover", (e) => {
